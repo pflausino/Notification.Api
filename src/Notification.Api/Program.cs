3b2784551c7642dd12aec9bdf12543af7df00df3
Paulo V Calderaro Flausino <calderaro95@hotmail.com>
2021-07-28 13:57:14 -0300

    Feature/01 estrutura inicial do projeto (#3)
    * Estrutura Inicial do projeto com Testes
    
    * Estrutura final do projeto

--- src/Notification.Api/Program.cs
+++ src/Notification.Api/Program.cs
@@ -20,7 +20,10 @@ namespace Notification.Api
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                        .UseIISIntegration()
+                        .UseKestrel();
+                    
                 });
     }
 }
